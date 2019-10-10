2c1db45ebb1c1d9822a8b842dbedc39506115444
nori <norinorimcz@gmail.com>
2019-10-10 19:34:16 +0900

    add sample data about SQLite

--- api/application/controllers/Index.php
+++ api/application/controllers/Index.php
@@ -2,19 +2,34 @@
 class Index extends CI_Controller {
     public function __construct() {
         parent::__construct();
-        $this->load->helper('url');
-        $this->load->helper('file');
     }
 
     public function index() {
-        // $data['file'] = $this->readFile();
-        // $data['file'] = [];
-        $data['file'] = read_file_to_array(base_url().'assets/python/img_list.txt');
+        //適当に作ってみたSQLiteのサンプル
 
-        /*******
-        render
-        *******/
-        $this->load->view('templates/header');
-        $this->load->view('index/index', $data);
+        $this->load->database();  // databaseクラスの呼び出し($this->db->???で使えるようになる)
+        $this->db->query('create table if not exists acount (id integer, name varchar)');   //クエリを実行。acount(int id, varchar name)テーブルが存在しなかったら作成する。
+
+        //insert文章の実行
+        $this->db->insert('acount', array(
+            'id' => 01,
+            'name' => 'Tom'
+            )
+        );
+
+        // select * from acountを出力
+        print('<pre>');
+        var_dump($this->db->query('select * from acount'));
+        print('</pre>');
+
+
+        /**
+         * もっといろんな方法があるみたいです。
+         * 
+         * 参考文献
+         * https://pgmemo.tokyo/data/archives/1077.html
+         * https://codeigniter.jp/user_guide/3/database/query_builder.html
+         */
+        
     }
 }
